f410884bea03292189fd5543632dbaf65249ef87
Jean de Dieu <jeandedieuam@gmail.com>
2018-12-27 14:55:07 +0200

    adding the Post/question endpoint in crudQuestion branch

--- api/routes/question.js
+++ api/routes/question.js
@@ -3,31 +3,31 @@ const router = express.Router();
 
 questions = [
     {
-        "id" : 1,
-        "createdOn" : "2018/12/30",
-        "createdBy" : 1,
-        "meetup" : 1,
-        "title" : "JS standardization",
-        "body" : "Why js syntaxes differ from a version to antoher?",
-        "votes" : 1
+        id : 1,
+        createdOn : "2018/12/30",
+        createdBy : 1,
+        meetup : 1,
+        title : "JS standardization",
+        body : "Why js syntaxes differ from a version to antoher?",
+        votes : 1
     },
     {
-        "id" : 2,
-        "createdOn" : "2018/12/26",
-        "createdBy" : 2,
-        "meetup" : 2,
-        "title" : "Algrorithm complexity",
-        "body" : "How to set the order of functions?",
-        "votes" : 1
+        id : 2,
+        createdOn : "2018/12/26",
+        createdBy : 2,
+        meetup : 2,
+        title : "Algrorithm complexity",
+        body : "How to set the order of functions?",
+        votes : 1
     },
     {
-        "id" : 3,
-        "createdOn" : "2018/12/27",
-        "createdBy" : 3,
-        "meetup" : 3,
-        "title" : "Expert system",
-        "body" : "What expert system consists on?",
-        "votes" : 1
+        id : 3,
+        createdOn : "2018/12/27",
+        createdBy : 3,
+        meetup : 3,
+        title : "Expert system",
+        body : "What expert system consists on?",
+        votes : 1
     },
 ];
 
@@ -38,9 +38,17 @@ router.get('/', (req, res, next)=> {
 
 router.post('/', (req, res, next)=> {
  
-    res.status(200).json({
-        message : 'posting a question'
-    })
+    const question = { 
+
+      id: questions.length +1,
+      user : req.body.user,
+      meetup : req.body.meetup,
+      title : req.body.title,
+      body : req.body.body
+    };
+   questions.push(question);
+   res.send(question);
+
 });
 
 router.get('/:questionId', (req,res,next)=>{
