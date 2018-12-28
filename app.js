581f3561bb76edfe8d549bd0725d4eeb6eaaac6c
Jean de Dieu <jeandedieuam@gmail.com>
2018-12-28 19:32:10 +0200

    updating question.js and meetup.js

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const userRoutes = require('./api/routes/user');
-const meetupRoutes = require('./api/routes/meetup');
-const questionRoutes = require('./api/routes/question');
+const meetupRoutes = require('./api/routes/meetups');
+const questionRoutes = require('./api/routes/questions');
 const rsvpRoutes = require('./api/routes/rsvp');
 
 app.use(morgan('dev'));
@@ -15,10 +15,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
-app.use('/user', userRoutes);
-app.use('/meetup', meetupRoutes);
-app.use('/question', questionRoutes);
-app.use('/rsvp', rsvpRoutes);
+app.use('/v1/users', userRoutes);
+app.use('/v1/meetups', meetupRoutes);
+app.use('/v1/questions', questionRoutes);
+app.use('/v1/rsvp', rsvpRoutes);
 
 
 module.exports = app;
