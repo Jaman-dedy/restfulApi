15b9b2222f85a40911822cf1d740c9fdb99f79a1
Jean de Dieu <jeandedieuam@gmail.com>
2018-12-29 19:05:46 +0200

    adapting files with related branch and updating app.js

--- app.js
+++ app.js
@@ -5,20 +5,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const userRoutes = require('./api/routes/user');
-const meetupRoutes = require('./api/routes/meetup');
 const questionRoutes = require('./api/routes/question');
-const rsvpRoutes = require('./api/routes/rsvp');
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
-app.use('/v1/users', userRoutes);
-app.use('/v1/meetups', meetupRoutes);
-app.use('/v1/questions', questionRoutes);
-app.use('/v1/rsvp', rsvpRoutes);
+app.use('/questions', questionRoutes);
 
 
 module.exports = app;
