8d550c32e3c0575478ce71d8a674ed74da251dc6
Jean de Dieu <jeandedieuam@gmail.com>
2019-01-03 19:19:44 +0200

    udpating files for unitest

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const userRoutes = require('./api/routes/user');
-const rsvpRoutes = require('./api/routes/rsvp');
-const questionRoutes = require('./api/routes/question');
-const meetupRoutes = require('./api/routes/meetup');
+// const rsvpRoutes = require('./api/routes/rsvp');
+// const questionRoutes = require('./api/routes/question');
+// const meetupRoutes = require('./api/routes/meetup');
 
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/questions', questionRoutes);
+// app.use('/questions', questionRoutes);
 app.use('/users', userRoutes);
-app.use('/rsvps', rsvpRoutes);
-app.use('/meetups', meetupRoutes);
+// app.use('/rsvps', rsvpRoutes);
+// app.use('/meetups', meetupRoutes);
 
 
 module.exports = app;
